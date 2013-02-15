6225de4ff5cecfc5bdd64f1bb168190bdc85edf2
Ricardo Alcocer <alco@ricardoalcocer.com>
2013-02-16 04:52:45 +0900

    Initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,29 @@
+// shows or hide the menu
+var menuOpen = false;
+function showhidemenu(e) {
+	if (!menuOpen) {
+		moveTo = "280dp";
+		menuOpen = true;
+	} else {
+		moveTo = "0";
+		menuOpen = false;
+	}
+
+	$.appMain.width = Ti.Platform.displayCaps.platformWidth;
+	$.appMain.animate({
+		left : moveTo,
+		duration : 200
+	});
+}
+//
+
+//
+Ti.Gesture.addEventListener('orientationchange', function(e) {
+    $.appMain.width=Ti.Platform.displayCaps.platformWidth;
+});
+//
+
+//
+Ti.API.info("Using theme: " + Alloy.CFG.theme);
+
+$.index.open();
